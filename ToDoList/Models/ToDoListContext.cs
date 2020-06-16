d1e9213d361c40adceb3c9b6366cc3c216114566
brian hensley <brnhensley@gmail.com>
2020-06-16 14:48:46 -0700

    3.5.0.4 - Many-to-Many Relationships: Join Entities

--- ToDoList/Models/ToDoListContext.cs
+++ ToDoList/Models/ToDoListContext.cs
@@ -4,8 +4,9 @@ namespace ToDoList.Models
 {
     public class ToDoListContext : DbContext
     {
-        public virtual DbSet<Item> Items { get; set; }
         public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Item> Items { get; set; }
+        public virtual DbSet<CategoryItem> CategoryItem { get; set; }
 
         public ToDoListContext(DbContextOptions options) : base(options) { }
     }
