0bf2bb62ac245efd524c77b42ecad6f0f3e97c19
Brian Hensley <brnhensley@gmail.com>
2020-12-03 08:59:52 -0800

    Account not Accounts

--- ToDoList/Controllers/AccountController.cs
+++ ToDoList/Controllers/AccountController.cs
@@ -6,13 +6,13 @@ using ToDoList.ViewModels;
 
 namespace ToDoList.Controllers
 {
-    public class AccountsController : Controller
+    public class AccountController : Controller
     {
         private readonly ToDoListContext _db;
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
 
-        public AccountsController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, ToDoListContext db)
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, ToDoListContext db)
         {
             _userManager = userManager;
             _signInManager = signInManager;
