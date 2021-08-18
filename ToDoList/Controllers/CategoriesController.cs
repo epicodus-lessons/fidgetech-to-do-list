e169c82f82a9e4c4e7d7da185201f2ae529887af
Brian Hensley <brnhensley@gmail.com>
2021-08-18 14:30:01 -0700

    3.3.1.6 - EF Core with an Existing Database

--- ToDoList/Controllers/CategoriesController.cs
+++ ToDoList/Controllers/CategoriesController.cs
@@ -1,48 +1,48 @@
-using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
-using System;
-using ToDoList.Models;
+// using Microsoft.AspNetCore.Mvc;
+// using System.Collections.Generic;
+// using System;
+// using ToDoList.Models;
 
-namespace ToDoList.Controllers
-{
-    public class CategoriesController : Controller
-    {
-        [HttpGet("/categories")]
-        public ActionResult Index()
-        {
-            List<Category> allCategories = Category.GetAll();
-            return View(allCategories);
-        }
+// namespace ToDoList.Controllers
+// {
+//     public class CategoriesController : Controller
+//     {
+//         [HttpGet("/categories")]
+//         public ActionResult Index()
+//         {
+//             List<Category> allCategories = Category.GetAll();
+//             return View(allCategories);
+//         }
 
-        [HttpGet("/categories/new")]
-        public ActionResult New()
-        {
-            return View();
-        }
+//         [HttpGet("/categories/new")]
+//         public ActionResult New()
+//         {
+//             return View();
+//         }
 
-        [HttpPost("/categories")]
-        public ActionResult Create(string categoryName)
-        {
-            Category newCategory = new Category(categoryName);
-            return RedirectToAction("Index");
-        }
+//         [HttpPost("/categories")]
+//         public ActionResult Create(string categoryName)
+//         {
+//             Category newCategory = new Category(categoryName);
+//             return RedirectToAction("Index");
+//         }
 
-        [HttpGet("/categories/{id}")]
-        public ActionResult Show(int id)
-        {
-            Category selectedCategory = Category.Find(id);
-            return View(selectedCategory);
-        }
+//         [HttpGet("/categories/{id}")]
+//         public ActionResult Show(int id)
+//         {
+//             Category selectedCategory = Category.Find(id);
+//             return View(selectedCategory);
+//         }
 
-        // This one creates new Items within a given Category, not new Categories:
-        [HttpPost("/categories/{categoryId}/items")]
-        public ActionResult Create(int categoryId, string itemDescription)
-        {
-            Category foundCategory = Category.Find(categoryId);
-            Item newItem = new Item(itemDescription);
-            newItem.Save();
-            foundCategory.AddItem(newItem);
-            return View("Show", foundCategory);
-        }
-    }
-}
+//         // This one creates new Items within a given Category, not new Categories:
+//         [HttpPost("/categories/{categoryId}/items")]
+//         public ActionResult Create(int categoryId, string itemDescription)
+//         {
+//             Category foundCategory = Category.Find(categoryId);
+//             Item newItem = new Item(itemDescription);
+//             newItem.Save();
+//             foundCategory.AddItem(newItem);
+//             return View("Show", foundCategory);
+//         }
+//     }
+// }
