491f46d0597f1d32812a36ce1e50286192c09920
Brian Hensley <brnhensley@gmail.com>
2021-09-09 10:59:26 -0700

    3.5.0.9 - Integrating Entity and Identity

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -1,24 +1,34 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
 using ToDoList.Models;
 
 namespace ToDoList.Controllers
 {
+    [Authorize]
     public class ItemsController : Controller
     {
         private readonly ToDoListContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public ItemsController(ToDoListContext db)
+        public ItemsController(UserManager<ApplicationUser> userManager, ToDoListContext db)
         {
+            _userManager = userManager;
             _db = db;
         }
 
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
-            return View(_db.Items.ToList());
+            string userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            ApplicationUser currentUser = await _userManager.FindByIdAsync(userId);
+            List<Item> userItems = _db.Items.Where(entry => entry.User.Id == currentUser.Id).ToList();
+            return View(userItems);
         }
 
         public ActionResult Details(int id)
@@ -37,17 +47,18 @@ namespace ToDoList.Controllers
         }
 
         [HttpPost]
-        public ActionResult Create(Item item, int categoryId)
+        public async Task<ActionResult> Create(Item item, int CategoryId)
         {
+            string userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            ApplicationUser currentUser = await _userManager.FindByIdAsync(userId);
+            item.User = currentUser;
             _db.Items.Add(item);
             _db.SaveChanges();
-
-            if (categoryId != 0)
+            if (CategoryId != 0)
             {
-                _db.CategoriesItems.Add(new CategoryItem() { CategoryId = categoryId, ItemId = item.ItemId });
+                _db.CategoriesItems.Add(new CategoryItem() { CategoryId = CategoryId, ItemId = item.ItemId });
                 _db.SaveChanges();
             }
-
             return RedirectToAction("Index");
         }
 
